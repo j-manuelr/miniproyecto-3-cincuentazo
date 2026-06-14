f40399689ca07c133653b64ee20b3db9fe63ba05
carlosk4 <lorenzohinestroza44@gmail.com>
2026-06-13 21:52:28 -0500

    vista básica gameview

--- src/main/java/np/cincuentazo/controller/GameController.java
+++ src/main/java/np/cincuentazo/controller/GameController.java
@@ -1,4 +1,15 @@
 package np.cincuentazo.controller;
 
+import javafx.fxml.FXML;
+
 public class GameController {
+
+    @FXML
+    private void onDeckClicked() {}
+
+    @FXML
+    private void onDrawCard() {}
+
+    @FXML
+    private void onNewGame() {}
 }
