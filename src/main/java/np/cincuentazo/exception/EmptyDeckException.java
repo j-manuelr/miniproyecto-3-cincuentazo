160d0a3004222fbb5ac6c844e9f1b0f021697c37
Natalia Andrea Parra Peña <natalia.andrea.parra@correounivalle.edu.co>
2026-06-15 16:15:42 -0500

    Implementación excepciones para el modelo

--- src/main/java/np/cincuentazo/exception/EmptyDeckException.java
+++ src/main/java/np/cincuentazo/exception/EmptyDeckException.java
@@ -1,4 +1,14 @@
 package np.cincuentazo.exception;
 
-public class EmptyDeckException {
+
+public class EmptyDeckException extends RuntimeException {
+
+
+    public EmptyDeckException(String message) {
+        super(message);
+    }
+
+    public EmptyDeckException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/main/java/np/cincuentazo/exception/InvalidPlayException.java
+++ src/main/java/np/cincuentazo/exception/InvalidPlayException.java
@@ -1,4 +1,34 @@
 package np.cincuentazo.exception;
 
-public class InvalidPlayException {
+import np.cincuentazo.model.Card;
+
+public class InvalidPlayException extends Exception{
+    private final Card attempetedCard;
+
+    private final int tableSum;
+
+    public InvalidPlayException(Card attempetedCard, int tableSum){
+        super(buildMessage(attempetedCard, tableSum));
+        this.attempetedCard = attempetedCard;
+        this.tableSum      = tableSum;
+    }
+
+    private static String buildMessage(Card card, int sum) {
+        int resultingSum = sum + card.getValue(sum);
+        return "Cannot play " + card + ": table sum would become " +
+                resultingSum + " (max allowed is " + Card.MAX_TABLE_SUM + "). " +
+                "Current sum: " + sum + ".";
+    }
+
+    /** @return the card that triggered this exception */
+    public Card getAttemptedCard() {
+        return attempetedCard;
+    }
+
+    /** @return the table sum at the time the illegal play was attempted */
+    public int getTableSum() {
+        return tableSum;
+    }
 }
+
+
